282ce98e01e2e70aa952bfaff877a2b586f56128
crawlic-stud <nikitosik0726@gmail.com>
2023-02-17 07:35:46 +0300

    added some palettes

--- main.py
+++ main.py
@@ -35,20 +35,20 @@ if __name__ == "__main__":
     random_animal = random.choice(animals)
 
     width, height = 1000, 1200
-    print(random_animal)
-    card = Card(random_animal, (width, height))
-    # card = Card("https://sun9-77.userapi.com/impg/r5KBGSWhIemSFRquNd5m4f4El_gYFQr6QBb1HQ/AwUp14Pn7sk.jpg?size=694x693&quality=96&sign=6115c30399f7e452dcede64ae91e0259&c_uniq_tag=rjg2sjwFq_sCuFzNAiVfHKIx7_k0ETVFqH2qTA4l4H8&type=album", (width, height))
+    # print(random_animal)
+    # card = Card(random_animal, (width, height))
+    card = Card("static/images/elmira.JPG", (width, height))
 
     card \
         .shape_image(Shape.HEART) \
         .change_palette( 
-            Palette(
-                (184, 238, 255),
-                (120, 248, 255),
-                (191, 255, 207),
-                (19, 127, 173),
-                (30, 129, 138),
-            )
+            # Palette(
+            #     (184, 238, 255),
+            #     (120, 248, 255),
+            #     (191, 255, 207),
+            #     (19, 127, 173),
+            #     (30, 129, 138),
+            # )
             # Palette(
             #     (255, 214, 234),
             #     (253, 222, 255),
@@ -56,15 +56,53 @@ if __name__ == "__main__":
             #     (138, 37, 99),
             #     (168, 57, 83),
             # )
+            # Palette(
+            #     (255, 232, 253),
+            #     (255, 112, 203),
+            #     (255, 145, 198),
+            #     (115, 18, 28),
+            #     (161, 34, 95),
+            # )
+            # Palette(
+            #     (255, 234, 186),
+            #     (252, 172, 204),
+            #     (255, 189, 158),
+            #     (194, 56, 17),
+            #     (217, 102, 20),
+            # )
+            # Palette(
+            #     (191, 234, 255),
+            #     (161, 255, 222),
+            #     (192, 228, 252),
+            #     (112, 115, 255),
+            #     (109, 166, 252),
+            # )
+            # Palette(
+            #     (252, 212, 239),
+            #     (252, 224, 220),
+            #     (252, 167, 185),
+            #     (217, 98, 139),
+            #     (240, 117, 144),
+            # )
+            Palette(
+                (255, 130, 174),
+                (255, 220, 173),
+                (252, 169, 157),
+                (255, 56, 79),
+                (235, 122, 70),
+            )
         ) \
         .change_background() \
         .add_outline(150) \
         .scale_image(0.75) \
-        .move_image(125) \
+        .move_image(150) \
         .set_font(Font.variant_5) \
-        .set_font_size(32) \
-        .add_title("я хочу дарувати тобi тепло кожен день!", padding=20) \
-        .add_text("дякую, що змушуеш мене посмiхатися i вiдчувати себе живою! ти робиш мое життя яскравим i незабутнiм! давай будемо поруч один з одним завжди!", 
+        .set_font_size(42) \
+        .set_padding_x(120) \
+        .add_title("Любимой тёте Эльмирочке!", padding=20) \
+        .set_padding_x(200) \
+        .add_text("Как много разных валентинок Кружится в снежном феврале. \nОдна из них — моя к тебе!", 
                   padding=0) \
-        .save_image("./static/images/result.png")
+        .save_image("./static/images/result.png") \
+        .show_image()
         
